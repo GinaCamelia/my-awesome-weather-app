4628cf04bbcb2281987e499e23642628547583d8
GinaCamelia <camelia_hodorogea@yahoo.com>
2021-12-05 12:13:45 +0000

    Update java.js

--- java.js
+++ java.js
@@ -83,7 +83,7 @@ function displayWeather(response) {
   let temperatureElement = document.querySelector(".temp");
   temperatureElement.innerHTML = `${temperature}`;
 
-  let iconElement = document.querySelector("#icon");
+  let iconElement = document.querySelector(".icon");
   iconElement.innerHTML = `${response.data.weather[0].icon}`; 
 
   let descriptionElement = document.querySelector(".description");
