b5bb40bb2e3f826e751e1080169b2e46c80e617a
GinaCamelia <camelia_hodorogea@yahoo.com>
2021-12-05 21:25:52 +0000

    Update java.js

--- java.js
+++ java.js
@@ -121,7 +121,7 @@ function displayWeather(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 
   let descriptionElement = document.querySelector(".description");
-  descriptionElement.innerHTML = response.data.weather[0].description;
+  descriptionElement.innerHTML = response.data.weather[0].main;
 
   let cityElement = document.querySelector(".city");
   cityElement.innerHTML = response.data.name;
