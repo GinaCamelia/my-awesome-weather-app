ab80b8ca67dbc0339ac938e5b67b3e98b45492d9
GinaCamelia <camelia_hodorogea@yahoo.com>
2021-12-05 16:48:01 +0000

    Update java.js

--- java.js
+++ java.js
@@ -95,7 +95,7 @@ function displayWeather(response) {
 
   let iconElement = document.querySelector("#icon");
   iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
-  iconElement.setAttribute("alt", response.data.weather[0].main);
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 
   let descriptionElement = document.querySelector(".description");
   descriptionElement.innerHTML = response.data.weather[0].main;
