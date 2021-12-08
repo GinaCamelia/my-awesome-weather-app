2a7d38e046c4356517a1d2744b3dd6fe91a19113
GinaCamelia <camelia_hodorogea@yahoo.com>
2021-12-08 11:14:22 +0000

    .

--- java.js
+++ java.js
@@ -60,8 +60,6 @@ function handleSubmit(event) {
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
-//displayCity("London");
-
 
 function convertToFahrenheit(event) {
   event.preventDefault();
