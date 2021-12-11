55337861195506ec81d1850f6461b9c2e946fb78
GinaCamelia <camelia_hodorogea@yahoo.com>
2021-12-11 16:44:17 +0000

    Update java.js

--- java.js
+++ java.js
@@ -87,7 +87,7 @@ function getForecast(coordinates) {
 console.log(coordinates);
 let apiKey = "baad171896e0c3b36f831a6990f30812";
 let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?
-lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=${units}`;
+lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}`;
 console.log(apiUrl);
 
 axios.get(apiUrl).then(displayForecast);
