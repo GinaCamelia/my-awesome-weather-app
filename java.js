81cbf3f93b5e27c5ed56753f8b27a00e7f2615e1
GinaCamelia <camelia_hodorogea@yahoo.com>
2021-12-11 16:42:57 +0000

    .

--- java.js
+++ java.js
@@ -87,7 +87,7 @@ function getForecast(coordinates) {
 console.log(coordinates);
 let apiKey = "baad171896e0c3b36f831a6990f30812";
 let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?
-lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=${metric}`;
 console.log(apiUrl);
 
 axios.get(apiUrl).then(displayForecast);
@@ -182,7 +182,7 @@ function currentLocation(position) {
   let key = "baad171896e0c3b36f831a6990f30812";
   let units = "metric";
   let url = `https://api.openweathermap.org/data/2.5/weather?
-  lat=${latitude}&lon=${longitude}&appid=${key}&units=${units}`;
+  lat=${latitude}&lon=${longitude}&appid=${key}&units=${metric}`;
   axios.get(url).then(displayWeather);
 }
 
@@ -193,3 +193,4 @@ function getCurrentPosition(event) {
 
 let button = document.querySelector(".btn-light");
 button.addEventListener("click", getCurrentPosition);
+
